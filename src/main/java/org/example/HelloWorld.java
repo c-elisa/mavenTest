c435677c57b68654e76a6055c3f769ae9c41dc62
c-elisa <elisacacace02@gmail.com>
2023-10-20 15:57:53 +0200

    Initial commit

---
+++ src/main/java/org/example/HelloWorld.java
@@ -0,0 +1,10 @@
+// This is a test for maven
+
+public class HelloWorld{
+
+    private static String s = "Hello World!";
+
+    public static void main(String args[]){
+        System.out.println(s);
+    }
+}
